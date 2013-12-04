31a553b30150efb45e990d9349da1b55d1f8077f
Jakub Kadlubiec <jakub.kadlubiec@gmail.com>
2013-12-04 02:30:45 +0100

    Initial commit

---
+++ ContosoUniversity/Models/Enrollment.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContosoUniversity.Models
+{
+    public enum Grade
+    {
+        A, B, C, D, F
+    }
+
+    public class Enrollment
+    {
+        public int EnrollmentID { get; set; }
+        [Display(Name = "Course")]
+        public int CourseID { get; set; }
+        [Display(Name = "Student")]
+        public int StudentID { get; set; }
+        [DisplayFormat(NullDisplayText = "No grade")]
+        public Grade? Grade { get; set; }
+
+        public virtual Course Course { get; set; }
+        public virtual Student Student { get; set; }
+    }
+}
